159eb35ee0ffac4bb3ac7880ac51a59dfca866f7
hakim <abdulhakim0401@gmail.com>
2024-11-20 01:30:35 +0800

    get rid of dark mode

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -10,60 +10,60 @@
       <div class="mx-auto px-6 max-w-6xl text-gray-500">
         <div class="relative">
           <div class="relative z-10 grid gap-3 grid-cols-6">
-            <div class="col-span-full lg:col-span-2 overflow-hidden flex relative p-8 rounded-xl bg-white border border-gray-200 dark:border-gray-800 dark:bg-gray-900">
+            <div class="col-span-full lg:col-span-2 overflow-hidden flex relative p-8 rounded-xl bg-white border border-gray-200 ">
               <div class="size-fit m-auto relative">
                 <div class="relative  flex items-center text-2xl font-semibold text-black">
                 Hello! Im Abdul Hakim
                   </div>
-                <h2 class="mt-6 text-center font-semibold text-gray-950 dark:text-white text-3xl">Software Developer</h2>
+                <h2 class="mt-6 text-center font-semibold text-gray-950  text-3xl">Software Developer</h2>
               </div>
             </div>
-            <div class="col-span-full sm:col-span-3 lg:col-span-2 overflow-hidden relative p-8 rounded-xl bg-white border border-gray-200 dark:border-gray-800 dark:bg-gray-900">
+            <div class="col-span-full sm:col-span-3 lg:col-span-2 overflow-hidden relative p-8 rounded-xl bg-white border border-gray-200 ">
               <div>
-                <div class="relative aspect-square rounded-full size-32 flex border mx-auto dark:bg-white/5 dark:border-white/10 before:absolute before:-inset-2 before:border dark:before:border-white/5 dark:before:bg-white/5 before:rounded-full">
+                <div class="relative aspect-square rounded-full size-32 flex border mx-auto   before:absolute before:-inset-2 before:border  before:rounded-full">
 
                 </div>
                 <div class="mt-6 text-center relative z-10 space-y-2">
-                  <h2 class="text-lg font-medium text-gray-800 transition group-hover:text-purple-950 dark:text-white">About Me</h2>
-                  <p class="dark:text-gray-300 text-gray-700">Provident fugit and vero voluptate. magnam magni doloribus dolores voluptates a sapiente nisi.</p>
+                  <h2 class="text-lg font-medium text-gray-800 transition group-hover:text-purple-950 ">About Me</h2>
+                  <p class=" text-gray-700">Provident fugit and vero voluptate. magnam magni doloribus dolores voluptates a sapiente nisi.</p>
                 </div>
               </div>
             </div>
-            <div class="col-span-full sm:col-span-3 lg:col-span-2 overflow-hidden relative p-8 rounded-xl bg-white border border-gray-200 dark:border-gray-800 dark:bg-gray-900">
+            <div class="col-span-full sm:col-span-3 lg:col-span-2 overflow-hidden relative p-8 rounded-xl bg-white border border-gray-200 ">
               <div>
                 <div class="pt-6 lg:px-6">
 
                 </div>
                 <div class="mt-14 text-center relative z-10 space-y-2">
-                  <h2 class="text-lg font-medium text-gray-800 transition group-hover:text-purple-950 dark:text-white">Tech Stack</h2>
-                  <p class="dark:text-gray-300 text-gray-700">Provident fugit vero voluptate. magnam magni doloribus dolores voluptates inventore nisi.</p>
+                  <h2 class="text-lg font-medium text-gray-800 transition group-hover:text-purple-950 ">Tech Stack</h2>
+                  <p class="text-gray-700">Provident fugit vero voluptate. magnam magni doloribus dolores voluptates inventore nisi.</p>
                 </div>
               </div>
             </div>
-            <div class="col-span-full lg:col-span-3 overflow-hidden relative p-8 rounded-xl bg-white border border-gray-200 dark:border-gray-800 dark:bg-gray-900">
+            <div class="col-span-full lg:col-span-3 overflow-hidden relative p-8 rounded-xl bg-white border border-gray-200 ">
               <div class="grid sm:grid-cols-2">
                 <div class="flex flex-col justify-between relative z-10 space-y-12 lg:space-y-6">
 
                   <div class="space-y-2">
-                    <h2 class="text-lg font-medium text-gray-800 transition group-hover:text-purple-950 dark:text-white">Faster than light</h2>
-                    <p class="dark:text-gray-300 text-gray-700">Provident fugit vero voluptate. Voluptates a sapiente inventore nisi.</p>
+                    <h2 class="text-lg font-medium text-gray-800 transition group-hover:text-purple-950 ">Faster than light</h2>
+                    <p class=" text-gray-700">Provident fugit vero voluptate. Voluptates a sapiente inventore nisi.</p>
                   </div>
                 </div>
 
               </div>
             </div>
-            <div class="col-span-full lg:col-span-3 overflow-hidden relative p-8 rounded-xl bg-white border border-gray-200 dark:border-gray-800 dark:bg-gray-900">
+            <div class="col-span-full lg:col-span-3 overflow-hidden relative p-8 rounded-xl bg-white border border-gray-200">
               <div class="h-full grid sm:grid-cols-2">
                 <div class="flex flex-col justify-between relative z-10 space-y-12 lg:space-y-6">
-                  <div class="relative aspect-square rounded-full size-12 flex border dark:bg-white/5 dark:border-white/10 before:absolute before:-inset-2 before:border dark:before:border-white/5 dark:before:bg-white/5 before:rounded-full">
+                  <div class="relative aspect-square rounded-full size-12 flex border before:absolute before:-inset-2 before:border  before:rounded-full">
 
                   </div>
                   <div class="space-y-2">
-                    <h2 class="text-lg font-medium text-gray-800 transition group-hover:text-purple-950 dark:text-white">Keep your loved ones safe</h2>
-                    <p class="dark:text-gray-300 text-gray-700">Voluptate. magnam magni doloribus dolores voluptates a sapiente inventore nisi.</p>
+                    <h2 class="text-lg font-medium text-gray-800 transition group-hover:text-purple-950 ">Keep your loved ones safe</h2>
+                    <p class="text-gray-700">Voluptate. magnam magni doloribus dolores voluptates a sapiente inventore nisi.</p>
                   </div>
                 </div>
-                <div class="mt-6 relative sm:-mr-[--card-padding] sm:-my-8 before:absolute before:w-px before:inset-0 before:mx-auto before:bg-gray-200 dark:before:bg-gray-800">
+                <div class="mt-6 relative sm:-mr-[--card-padding] sm:-my-8 before:absolute before:w-px before:inset-0 before:mx-auto before:bg-gray-200 ">
                   <div class="relative space-y-6 py-6 flex flex-col justify-center h-full">
 
 
