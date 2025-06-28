7fa31eafad8c26f3559ff3ec2d8d3560e412d750
hakim <abdulhakim0401@gmail.com>
2025-06-29 00:41:01 +0800

    Learn on useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-
-import './App.css'
-import Header from './sections/Header.jsx';
+import "./App.css";
+import Header from "./sections/Header.jsx";
 import Skill from "./sections/Skill.jsx";
 import About from "./sections/About.jsx";
 import Education from "./sections/Education.jsx";
@@ -9,24 +8,17 @@ import Projects from "./sections/Projects.jsx";
 import Navbar from "./components/Navbar.jsx";
 
 function App() {
-  
-
   return (
-
-   <div className='mx-auto min-h-screen max-w-2xl  px-6 py-12 '>
-    Yea
-       <Navbar/>
-   <Header/>
-    <About/>
-       <Skill/>
-       <Projects/>
-       <Education/>
-       <Footer/>
-
-
-   </div>
-
-  )
+    <div className="mx-auto min-h-screen max-w-2xl  px-6 py-12 ">
+      <Navbar />
+      <Header name="Hakim" />
+      <About />
+      <Skill />
+      <Projects />
+      <Education />
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,17 +1,23 @@
 import selfie from "../assets/selfie.jpg";
+import { useState } from "react";
+export default function Header(props) {
 
-export default function Header() {
+    const [goofy, setGoofy] = useState(true);
+  function handleClick() {
+        setGoofy(!goofy);   
+  }
     return (
         <div className="container mx-auto px-5 max-w-4xl">
             <div className="">
                 <img src={selfie} alt="Profile" className="rounded-full object-cover w-32 h-32 md:w-48 md:h-48" />
             </div>
             <h1 className="text-black pt-9 font-semibold text-3xl md:text-5xl">
-                Hi, I'm <span className="text-blue-700"> Hakim👋 </span>
+                Hi, I'm <span className="text-blue-700"> {props.name}👋 </span>
             </h1>
             <p className="text-black text-lg md:text-xl">
                 Software developer  wannabe who loves to build things and learn new stuff.
             </p>
+            <button className="bg-green-400 p-2 rounded-xl text-white" onClick={handleClick}>{goofy ? 'Professional Mode' : 'Goofy ahh Mode'}</button>
         </div>
     );
 }
