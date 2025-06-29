c7aeaca5b9d95f003af6b9420f56e211528d5ac0
hakim <abdulhakim0401@gmail.com>
2025-06-30 01:57:35 +0800

    Added navbar using props

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,26 @@
-export default function Navbar() {
+ function NavTabs(props) {
+    return (
+      <div>
+        <a href="#" className="text-gray-700 transition delay-50 duration-200 ease-in-out hover:bg-green-200 rounded-xl hover:scale-120 px-4 py-2">
+          {props.name}
+        </a>
+      </div>
+    )
+  }
+ 
+ function Navbar() {
+
+  
     return (
       <>
+      <div className="flex justify-center bg-gray-100 p-2 shadow-md">
+      <NavTabs name="Home" />
+      <NavTabs name="About" />
+      <NavTabs name="Contact" />
+      </div>
       </>
     );
 }
+
+export default Navbar; 
+
